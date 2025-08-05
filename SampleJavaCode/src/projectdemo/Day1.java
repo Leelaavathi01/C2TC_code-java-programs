e5b08fd505c881d0917d1eac7bab9c514fb23e4a
user <user@LAPTOP-60O9OP1D>
2025-08-05 08:31:00 +0430

    Updated

--- SampleJavaCode/src/projectdemo/Day1.java
+++ SampleJavaCode/src/projectdemo/Day1.java
@@ -3,8 +3,10 @@ package projectdemo;
 public class Day1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int a=2;
+		int b=3;
+		int sum=a+b;
+		System.out.println(sum);
 	}
 
 }
