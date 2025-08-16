e5893cbae4e6ed71beb8e7af5c1285a2fc0d6853
user <user@LAPTOP-60O9OP1D>
2025-08-16 09:43:45 +0430

    I am completed day3

--- SampleJavaCode/src/day3/ProductDemo.java
+++ SampleJavaCode/src/day3/ProductDemo.java
@@ -8,13 +8,16 @@ public class ProductDemo {
 		// TODO Auto-generated method stub
 		product p1=new product();
 		product p2=new product(281,"hai",45.8f);
+		
 		Scanner s=new Scanner(System.in);
 		System.out.println("enter the product details:");
+		
 		int sid=s.nextInt();
 		s.nextLine();
 		String prod_name=s.nextLine();
 		float price=s.nextFloat();
 		product p3=new product(sid,prod_name,price);
+		
 		System.out.println(p1);
 		System.out.println(p2);
 		System.out.println(p3);
