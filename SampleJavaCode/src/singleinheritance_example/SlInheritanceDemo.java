c2cf16bea24e849b184f0cee1a773c0036aa8e89
user <user@LAPTOP-60O9OP1D>
2025-08-19 13:10:38 +0430

    single inheritance uploaded

---
+++ SampleJavaCode/src/singleinheritance_example/SlInheritanceDemo.java
@@ -0,0 +1,14 @@
+package singleinheritance_example;
+
+public class SlInheritanceDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Trainer t=new Trainer(112,"leelaa","puducherry",11,"icb","mathagadipat");
+				System.out.println(t);
+				Person p=new Person(223,"tata","chennai");
+				System.out.println(p);
+
+	}
+
+}
