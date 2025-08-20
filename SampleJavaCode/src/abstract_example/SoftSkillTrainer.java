e6d53fac0d5ce13afc368e03df11c5a8d05f1e11
user <user@LAPTOP-60O9OP1D>
2025-08-20 13:08:47 +0430

    abstract example

---
+++ SampleJavaCode/src/abstract_example/SoftSkillTrainer.java
@@ -0,0 +1,11 @@
+package abstract_example;
+
+public class SoftSkillTrainer extends Trainer {
+	public void session() {
+		System.out.println("resume building");
+	}
+	public void checks() {
+		System.out.println("cover letter");
+	}
+
+}
---
+++ SampleJavaCode/src/abstract_example/TnsifCgDemo.java
@@ -0,0 +1,11 @@
+package abstract_example;
+
+public class TnsifCgDemo {
+	
+	public static void main(String[] args) {
+		Trainer t=new SoftSkillTrainer();
+		t.session();
+		t.checks();
+	}
+
+}
